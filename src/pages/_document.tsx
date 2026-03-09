a941dae672ccbd88f71ebfba7e95b8805f0b1056
Riccardo <rickyferrari05@gmail.com>
2026-03-09 15:35:38 +0100

    Fix home-screen icons: disable maskable cropping

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,10 +10,11 @@ export default function Document() {
         <meta name="apple-mobile-web-app-title" content="TRAINED" />
         <meta name="theme-color" content="#050507" />
         <link rel="manifest" href="/manifest.webmanifest" />
-        <link rel="icon" href="/icon-192.png?v=5" sizes="192x192" type="image/png" />
-        <link rel="icon" href="/icon-512.png?v=5" sizes="512x512" type="image/png" />
-        <link rel="shortcut icon" href="/icon-192.png?v=5" type="image/png" />
-        <link rel="apple-touch-icon" href="/icon-192.png?v=5" />
+        <link rel="icon" href="/icon-192.png?v=6" sizes="192x192" type="image/png" />
+        <link rel="icon" href="/icon-512.png?v=6" sizes="512x512" type="image/png" />
+        <link rel="shortcut icon" href="/icon-192.png?v=6" type="image/png" />
+        <link rel="apple-touch-icon" href="/icon-192.png?v=6" />
+        <link rel="apple-touch-icon-precomposed" href="/icon-192.png?v=6" />
       </Head>
       <body>
         <Main />
